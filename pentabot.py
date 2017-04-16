467f61cf14d94eabd9e0f15446fb7ca2daf01f3c
ooouuuuuuii <budderface9999@gmail.com>
2017-04-16 15:49:35 -0500

    Update pentabot.py
    Put breaks and notifications into code

--- pentabot.py
+++ pentabot.py
@@ -5,9 +5,9 @@ import praw
 penta_bot = praw.Reddit(user_agent='PentaBot', client_id='4yX0om_uGfOuhw', client_secret='WF5iZjc5Cjmg8T5OjqvJHQTG2jY', username='PentaBot', password='00poiuytrewq00')
 
 #Setting up
-subreddit = bot.subreddit('Diepio')
+subreddit = penta_bot.subreddit('Diepio')
 comments = subreddit.stream.comments()
-submissions = subreddit.stream.submissions()
+#submissions = subreddit.stream.submissions()
 
 #So I don't have to repeat it all the time :3
 penta = 'pentabot, '
@@ -17,20 +17,33 @@ for comment in comments:
 		text = comment.body
 		author = comment.author
 		if penta + 'git gud' in text.lower():
-			#Replies
+			#Notifies reply being sent
+			print("Replying to: {}".format(comment.title))
+			#Actually replies
 			comment.reply("I got gud, skreb.")
+			#Makes sure that bot doesn't repeat itself
+			break
 		elif penta + 'penta' in text.lower():
 			comment.reply("So balanced '3'.")
+			break
 		elif penta + 'overlord' in text.lower():
 			comment.reply("NEEERFFFF!")
+			break
 		elif penta + 'team' in text.lower():
 			comment.reply("*Spins*")
+			break
 		elif penta + 'destroyer' in text.lower():
 			comment.reply("NOOO! MY ONLY WEAKNESS!")
+			break
 		elif penta + 'automod' in text.lower():
 			comment.reply("I hope Automod-sama notices me...")
+			break
 		elif penta + 'hi' in text.lower():
-			comment.reply("Hi ")
+			if author == 'ooouuuuuii':
+				comment.reply("Hi dad :3")
+			else:
+				comment.reply("Hi u/{0}!".format(author))
+			break
 
 #Post checker
 #for submission in submissions:
