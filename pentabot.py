3304e22868676d2d7d0efd6ce621464afe9fe7fe
ooouuuuuuii <budderface9999@gmail.com>
2017-04-18 22:51:03 -0500

    Update pentabot.py
    Prep to show to Reddit a little.

--- pentabot.py
+++ pentabot.py
@@ -2,7 +2,7 @@
 import praw
 
 #Creates instance of bot
-penta_bot = praw.Reddit(user_agent='PentaBot', client_id='4yX0om_uGfOuhw', client_secret='WF5iZjc5Cjmg8T5OjqvJHQTG2jY', username='PentaBot', password='00poiuytrewq00')
+penta_bot = praw.Reddit(user_agent='PentaBot', client_id='4yX0om_uGfOuhw', client_secret='SEEECREET', username='PentaBot', password='NOT TELLING YOU LOL')
 
 #Setting up
 subreddit = penta_bot.subreddit('Diepio')
@@ -13,104 +13,89 @@ comments = subreddit.stream.comments()
 penta = 'pentabot, '
 
 #Comment checker
-for comment in comments:
-		text = comment.body
-		author = comment.author
-		if penta + 'git gud' in text.lower():
-			#Notifies reply being sent
-			print("Replying to: " + comment.title)
-			#Actually replies
-			comment.reply("I got gud, skreb.")
-			#Makes sure that bot doesn't repeat itself
-			break
-		elif penta + 'penta' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("So balanced '3'.")
-			break
-		elif penta + 'overlord' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("NEEERFFFF!")
-			break
-		elif penta + 'team' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("*Spins*")
-			break
-		elif penta + 'destroyer' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("Dodgeball is fun!")
-			break
-		elif penta + 'automod' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("I hope Automod-sama notices me...")
-			break
-		elif penta + 'hi' in text.lower():
-			if author == 'ooouuuuuii':
-				print("Replying to you. Why are you doing that?")
-				comment.reply("Hi dad :3")
-			else:
+while True:
+	for comment in comments:
+			text = comment.body
+			author = comment.author
+			if penta + 'git gud' in text.lower():
+				#Notifies reply being sent
 				print("Replying to: " + comment.title)
-				comment.reply("Hi u/{0}!".format(author))
-			break
-		elif penta + 'spread' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("My poor, poor retarted brother.")
-			break
-		elif penta + 'destroy' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("YES MASTER.")
-			break
-		elif penta + 'suggestion' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("If you have a suggestion about me, you can PM /u/ooouuuuuii about it. I also mentioned him just now, so you can comment here too.")
-			break
-		elif penta + 'necromancer' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("I love me a little snack sometimes!")
-			break
-		elif penta + 'up up down down left right left right b a start' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("PentaBot has now evolved to TeraBot!")
-			break
-		elif penta + 'booster' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("Even faster than me!")
-			break
-		elif penta + 'spam' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("oooooookaaaaaaaaayayyyuyuyyuyiuwyiruweyiruewifjbgiwubwbiewuebvwiejvewuijniewuvgbnswaiubrvkizjrgeqlgipqibqh0843tuqevnjlakerjbgqoajfaj")
-			break
-		elif penta + '日本語' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("すみません。　少し　日本語を　話します。")
-			break
-		elif penta + 'up up down down left right left right b a start' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("PentaBot has now evolved to TeraBot!")
-			break
-		elif penta + 'buff' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("*squealing*")
-			break
-		elif penta + 'hap' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("HAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAP")
-			break
-		elif penta + 'noob' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("U WOT M8")
-			break
-		elif penta + 'use thunderbolt' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("PENTAAA... BOOOOOOOOOOOOOOOOT!")
-			break
-		elif penta + 'predator' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("NOOOOO! MY ONLY WEEEAKNEEESSSS!")
-			break
-		elif penta + 'fanboy' in text.lower():
-			print("Replying to: " + comment.title)
-			comment.reply("*Changes font to Ubuntu Bold*")
-			break
+				#Actually replies
+				comment.reply("I got gud, skreb.")
+				#Makes sure that bot doesn't repeat itself
+				break
+			elif penta + 'penta' in text.lower():
+				comment.reply("So balanced '3'.")
+				break
+			elif penta + 'overlord' in text.lower():
+				comment.reply("NEEERFFFF!")
+				break
+			elif penta + 'team' in text.lower():
+				comment.reply("*Spins*")
+				break
+			elif penta + 'destroyer' in text.lower():
+				comment.reply("Dodgeball is fun!")
+				break
+			elif penta + 'automod' in text.lower():
+				comment.reply("I hope Automod-sama notices me...")
+				break
+			elif penta + 'hi' in text.lower():
+				if author == 'ooouuuuuii':
+					comment.reply("Hi dad :3")
+				else:
+					comment.reply("Hi u/{0}!".format(author))
+				break
+			elif penta + 'spread' in text.lower():
+				comment.reply("My poor, poor retarted brother.")
+				break
+			elif penta + 'suggestion' in text.lower():
+				comment.reply("If you have a suggestion about me, you can PM /u/ooouuuuuii about it. I also mentioned him just now, so you can comment here too.")
+				break
+			elif penta + 'necromancer' in text.lower():
+				comment.reply("I love me a little snack sometimes!")
+				break
+			elif penta + 'up up down down left right left right b a start' in text.lower():
+				comment.reply("PentaBot has now evolved to TeraBot!")
+				break
+			elif penta + 'booster' in text.lower():
+				comment.reply("Even faster than me!")
+				break
+			elif penta + 'spam' in text.lower():
+				comment.reply("oooooookaaaaaaaaayayyyuyuyyuyiuwyiruweyiruewifjbgiwubwbiewuebvwiejvewuijniewuvgbnswaiubrvkizjrgeqlgipqibqh0843tuqevnjlakerjbgqoajfaj")
+				break
+			elif penta + 'up up down down left right left right b a start' in text.lower():
+				comment.reply("PentaBot has now evolved to TeraBot!")
+				break
+			elif penta + 'buff' in text.lower():
+				comment.reply("*squealing*")
+				break
+			elif penta + 'hap' in text.lower():
+				comment.reply("HAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAPHAP")
+				break
+			elif penta + 'noob' in text.lower():
+				comment.reply("U WOT M8")
+				break
+			elif penta + 'use thunderbolt' in text.lower():
+				comment.reply("PENTAAA... BOOOOOOOOOOOOOOOOT!")
+				break
+			elif penta + 'predator' in text.lower():
+				comment.reply("NOOOOO! MY ONLY WEEEAKNEEESSSS!")
+				break
+			elif penta + 'fanboy' in text.lower():
+				comment.reply("*Changes font to Ubuntu Bold*")
+				break
+			elif penta + 'hate' in text.lower():
+				comment.reply("**TRIGGERED**")
+				break
+			#Commands inspired by others start here
+			elif penta + 'fuck diep.io fuck fuckfcukfcyif fuck this fucking game fuckf uck' in text.lower():
+				#thnx, /u/OvertrapperFTW
+				comment.reply("so fucking rage")
+				break
+			elif penta + 'cancer' in text.lower():
+				#thnx, /u/sbk2015
+				comment.reply("[MG]xXminecraftkid_pro_sipn2taemXx")
+				break
 
 #Post checker
 #for submission in submissions:
